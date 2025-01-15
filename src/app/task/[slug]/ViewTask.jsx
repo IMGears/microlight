50ef2d3918291cb07d23854614aec9d111526a49
Alex J V <alexjv89@gmail.com>
2025-01-15 07:54:03 +0530

    Basic task view page

--- src/app/task/[slug]/ViewTask.jsx
+++ src/app/task/[slug]/ViewTask.jsx
@@ -1,5 +1,56 @@
-export default function ViewTask({params,searchParams}){
-  return <>
-    view Task - {params.slug}
-  </>
+import { Container, Typography, Box } from '@mui/joy';
+import PageHeader from '@/components/PageHeader';
+import SendIcon from '@mui/icons-material/Send';
+
+function generateBreadcrumbs({task}) {
+  let breadcrumbs = [
+    {
+      text: "Library",
+      href: "/library"
+    }
+  ];
+
+  // Add task path segments to breadcrumbs if available
+  if (task._folderPath) {
+    const f_path = task._folderPath.split('/');
+    let folderPath = '/library';
+    
+    f_path.forEach((folder, index) => {
+      folderPath += '/' + folder;
+      breadcrumbs.push({
+        text: folder,
+        href: folderPath
+      });
+    });
+    breadcrumbs.push({
+      text:task.slug
+    })
+  }
+
+  return breadcrumbs;
+}
+
+export default function ViewTask({params, task, searchParams}) {
+  const breadcrumbs = generateBreadcrumbs({task});
+
+  return (
+    <Container>
+      <br/>
+      <PageHeader 
+        breadcrumbs={breadcrumbs} 
+        header={{
+          part1: 'Task:',
+          part2: task.name
+        }}
+        icon={<SendIcon sx={{color: '#6435c9'}} />}
+      />
+      
+      <Typography level="body-sm">{task.description}</Typography>
+
+      {/* Add your task execution UI components here */}
+      
+      {/* Uncomment for debugging */}
+      <pre>{JSON.stringify(task, null, 2)}</pre>
+    </Container>
+  );
 }
--- src/app/task/[slug]/page.jsx
+++ src/app/task/[slug]/page.jsx
@@ -1,6 +1,30 @@
 import ViewTask from "./ViewTask";
+import { notFound } from 'next/navigation';
 
-export default async function Page({params,searchParams}){
+async function getTaskDetails({params}) {
+  try {
+    const fullPath = require.resolve(`@/tasks/${params.slug}.task.js`);
+    
+    // Extract just the part after /tasks/ and clean up the path
+    const folderPath = fullPath
+      .split('/tasks/')[1]                    // Get everything after /tasks/
+      .split(`/${params.slug}.task.js`)[0];   // Remove the " [app-rsc] (ecmascript)" part
+    
+    const taskConfig = await import(`@/tasks/${params.slug}.task.js`);
+    return {
+      ...taskConfig.default,
+      _folderPath: folderPath
+    };
+  } catch(e) {
+    if (e.code === 'MODULE_NOT_FOUND') 
+      notFound();
+    throw e;
+  }
+}
+
+export default async function Page({params, searchParams}){
   params = await params;
-  return <ViewTask params={params} searchParams={searchParams}/>
+  const task = await getTaskDetails({params});
+  
+  return <ViewTask params={params} task={task} searchParams={searchParams}/>
 }
